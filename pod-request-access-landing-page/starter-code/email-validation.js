903832994c29de4139f3fb7bebd763ff292287b6
natiakhokhob <114824876+natiakhokhob@users.noreply.github.com>
2023-03-07 16:57:18 +0400

    master

---
+++ pod-request-access-landing-page/starter-code/email-validation.js
@@ -0,0 +1,22 @@
+const input = document.getElementById("email");
+const error = document.getElementsByClassName("error")[0];
+const button = document.getElementById("button");
+
+
+button.addEventListener("click", () => {
+  event.preventDefault();
+  emailValidation(input, error);
+});
+
+function emailValidation(inputText, errorr) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (inputText.value.match(mailformat)){
+        error.style.display = "none";
+        input.style.borderColor = "black";
+        return true;
+    } else {
+        error.style.display = "flex";
+        input.style.borderColor = "red";
+        return false;
+    }
+}
